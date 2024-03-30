0edf9ac64d4f3c902910a0b9182cf01ca674e282
Ixtron <65489825+Ixtron@users.noreply.github.com>
2024-03-30 14:03:41 +0400

    lesson 3

--- 2.py
+++ 2.py
@@ -1,4 +1,19 @@
-import numpy as np
-a = [1,2,3]
-b = np.array([1,2,3])
-print(a+a,b+b)
+class Dog:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def sit(self):
+        print(f"{self.name} сел")
+
+    def roll_over(self):
+        print(f"{self.name} перекатывается")
+
+
+my_dog = Dog("Ромка", 3)
+my_dog.sit()
+my_dog.roll_over()
+
+my_dog = Dog("Димка", 6)
+my_dog.sit()
+my_dog.roll_over()
